82d29134a33e4ce7d809d79bb9eb280f97eb8eb9
Thim <timhaselaars@gmail.com>
2026-02-13 08:18:43 +0100

    Add Cask for TypePaste application

---
+++ Casks/typepaste.rb
@@ -0,0 +1,11 @@
+cask "typepaste" do
+  version "0.0.0"
+  sha256 "REPLACE_ME"
+
+  url "https://github.com/trinixlabs/typepaste/releases/download/#{version}/TypePaste-#{version}.dmg"
+  name "TypePaste"
+  desc "Instant typing from your clipboard"
+  homepage "https://github.com/trinixlabs/typepaste"
+
+  app "TypePaste.app"
+end
